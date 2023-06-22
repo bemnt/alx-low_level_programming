889e4335778b5e456afa7b8a1598a095047d1689
bemnt <bemnt7070@gmail.com>
2023-06-22 14:03:45 -0700

    hi

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,18 @@
 #include "main.h"
+
 /**
- * print_line - Print line dependent on the integer n.
- * @n : The number of '_' characters to use
- * Return: Void.
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
+ * Return: no return.
  */
 void print_line(int n)
 {
 	int i;
-        if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
+
 	for (i = 0; i < n; i++)
 	{
-	_putchar(95);
+		_putchar(95);
 	}
 	_putchar('\n');
-        }
 }
 
